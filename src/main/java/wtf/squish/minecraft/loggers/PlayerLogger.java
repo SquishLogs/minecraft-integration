8886a0676de7d53bfc6167e688a77668eb427cff
Livaco <livaco@livaco.dev>
2023-01-08 16:42:21 +0000

    Some more player logs

--- src/main/java/wtf/squish/minecraft/loggers/PlayerLogger.java
+++ src/main/java/wtf/squish/minecraft/loggers/PlayerLogger.java
@@ -17,6 +17,10 @@ import java.awt.*;
  * @author Livaco
  */
 public class PlayerLogger implements Listener {
+    /**
+     * Logs the player joining the server, as well as calls for them to be registered.
+     * @param event The event.
+     */
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         SquishLogs.registerPlayer(event.getPlayer());
@@ -25,6 +29,32 @@ public class PlayerLogger implements Listener {
         new Log("Player | Connect")
                 .addFragment(event.getPlayer())
                 .addFragment("joined the server.")
+                .send();
+    }
+
+    /**
+     * Logs the player leaving the server.
+     * @param event The event.
+     */
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        new Log("Player | Disconnect")
+                .addFragment(event.getPlayer())
+                .addFragment("left the server.")
+                .send();
+    }
+
+    /**
+     * Logs the player chatting.
+     * @param event The event.
+     */
+    @EventHandler
+    public void onAsyncPlayerChat(AsyncPlayerChatEvent event) {
+        new Log("Player | Chat")
+                .addFragment(event.getPlayer())
+                .addFragment("said in chat")
+                .addFragment(event.getMessage(), Color.MAGENTA)
+                .addFragment(".")
                 .send();
     }
 }
