de2a27ddbbd4e58996f19366b03232e2dd6b605e
Livaco <livaco@livaco.dev>
2023-01-10 07:24:42 +0000

    PvP and generic death events

--- src/main/java/wtf/squish/minecraft/loggers/CombatLogger.java
+++ src/main/java/wtf/squish/minecraft/loggers/CombatLogger.java
@@ -3,7 +3,9 @@ package wtf.squish.minecraft.loggers;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import wtf.squish.minecraft.SquishLogs;
 import wtf.squish.minecraft.entities.Log;
 
@@ -18,8 +20,8 @@ public class CombatLogger implements Listener {
      */
     @EventHandler
     public void onEntityDeath(EntityDeathEvent event) {
-        if(event.getEntity().getKiller() == null) return;
         if(event.getEntity() instanceof Player) return;
+        if(event.getEntity().getKiller() == null) return;
 
         new Log("Combat | Entity Death")
                 .addFragment(event.getEntity().getKiller())
@@ -29,5 +31,27 @@ public class CombatLogger implements Listener {
                 .addFragment(event.getEntity().getKiller().getInventory().getItemInMainHand().getType().name().toLowerCase(), SquishLogs.getLogColor())
                 .addFragment(".")
                 .send();
+    }
+
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent event) {
+        Player player = event.getEntity();
+        Player killer = event.getEntity().getKiller();
+
+        if(killer != null) {
+            new Log("Combat | PvP")
+                    .addFragment(killer)
+                    .addFragment(" killed ")
+                    .addFragment(player)
+                    .addFragment(" with a ")
+                    .addFragment(killer.getInventory().getItemInMainHand().getType().name().toLowerCase(), SquishLogs.getLogColor())
+                    .addFragment(".")
+                    .send();
+        } else {
+            new Log("Combat | Death")
+                    .addFragment(player)
+                    .addFragment(" died.")
+                    .send();
+        }
     }
 }
